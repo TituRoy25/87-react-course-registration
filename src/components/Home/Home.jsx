b51d4a3ee0e5dd5402702a48a6c29035dcf1b832
TituRoy25 <tituroydinajpur11@gmail.com>
2023-09-15 03:00:12 +0600

    static card section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,19 +5,19 @@ import "./Home.css";
 
 const Home = () => {
     return (
-        <div className='container'>
-            <div className="card-container">
+        <div className='container mt-8'>
+            <div className="card-container px-5">
                 <div className="card p-2">
                     <div className="card-img">
                         <img className='photo' src="https://i.ibb.co/Wxv0V3F/Rectangle-2-2.png" alt="" />
                     </div>
-                    <h2>Introduction to C Programming</h2>
-                    <p><small>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</small></p>
-                    <div className="info">
-                        <p>Price:</p>
+                    <h2 className='font-semibold mt-2'>Introduction to C Programming</h2>
+                    <p className='mt-2'><small>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</small></p>
+                    <div className="info mt-2">
+                        <p>$ Price:</p>
                         <p>Credit:</p>
                     </div>
-                    <button>Select</button>
+                    <button className='bg-[#2F80ED] w-[232px] text-white mt-2'>Select</button>
                 </div>
             </div>           
         </div>
