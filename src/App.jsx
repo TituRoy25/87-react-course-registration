6214e310013a849bbcea2e08b5435a1e6c82ab08
TituRoy25 <tituroydinajpur11@gmail.com>
2023-09-14 23:53:35 +0600

    assignment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-slate-600'>Course Registration</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
