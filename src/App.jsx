ea8c395a9143ef499922ef65be667ba7e3819afb
TituRoy25 <tituroydinajpur11@gmail.com>
2023-09-16 21:53:29 +0600

    my project done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-6xl text-center font-bold py-5'>Course Registration</h1>
+      <h1 className='text-6xl text-center font-bold py-5'>Courses Registration</h1>
       <Home></Home>
       
       
